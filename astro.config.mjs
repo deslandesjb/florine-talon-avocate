60aa8322710b0e76572d3e3646c7d3ae934f7e32
Jb <deslandes.jb@gmail.com>
2025-11-30 01:13:53 +0100

    vercel added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
